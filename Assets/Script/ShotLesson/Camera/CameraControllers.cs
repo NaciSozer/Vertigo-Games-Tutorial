bbc2e413c78713a922e0651935d81da54f64076a
Naci Sözer <raci.fb.1907@gmail.com>
2021-11-08 23:25:30 +0300

    *Creat Input system *Update Camera system

---
+++ Assets/Script/ShotLesson/Camera/CameraControllers.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShotLesson.Camera
+{
+
+public class CameraControllers : MonoBehaviour
+{
+    [SerializeField] CameraSettings _cameraSettings;
+    [SerializeField] private Transform _targetTransform;
+    [SerializeField] private Transform _cameraTransform;
+    
+    private void Update()
+    {
+        CameraRotationFollow();
+        CameraPositionFollow();
+    }
+
+    private void CameraRotationFollow()
+    {
+        _cameraTransform.rotation = Quaternion.Lerp(
+            _cameraTransform.rotation,
+            Quaternion.LookRotation(_targetTransform.position - _cameraTransform.position),
+            Time.deltaTime * _cameraSettings.RotationSpeed);
+    }
+
+    private void CameraPositionFollow()
+    {
+        _cameraTransform.position = Vector3.Lerp(_cameraTransform.position,
+            _targetTransform.position + _cameraSettings.PositionOffset,
+           Time.deltaTime * _cameraSettings.PositionLerpSpeed);
+
+
+    }
+
+
+}
+}
