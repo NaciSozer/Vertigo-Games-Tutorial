f3490515cf6f3c223fcf9e9254df0cfff2c60fed
Naci Sözer <raci.fb.1907@gmail.com>
2021-11-08 21:44:53 +0300

    *Creat camera script *Update camera sprit

---
+++ Assets/Script/ShotLesson/CameraSettings.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = ("ShotLesson/Camera/Camera Settings"))]
+public class CameraSettings : ScriptableObject
+{
+
+    [Header("Rotation")]
+    [SerializeField] private float _rotationLerpSpeed;
+
+    public float RotationSpeed{get{ return _rotationLerpSpeed; } }
+
+
+    [Header("Position")]
+    [SerializeField] private Vector3 _positionOffset;
+
+    public Vector3 PositionOffset { get { return _positionOffset; } }
+
+
+    [SerializeField] private float _positionLerpSpeed;
+    public float PositionLerpSpeed { get { return _positionLerpSpeed; } }
+
+}
