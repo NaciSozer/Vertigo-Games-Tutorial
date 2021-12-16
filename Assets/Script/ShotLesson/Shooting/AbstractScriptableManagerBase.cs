cc941fdcebb514cad8872a24398dad62f3a2d4d2
Naci Sözer <raci.fb.1907@gmail.com>
2021-12-16 22:39:02 +0300

    *Creat Shoot system base

---
+++ Assets/Script/ShotLesson/Shooting/AbstractScriptableManagerBase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ShotLesson
+{
+    public class AbstractScriptableManagerBase : ScriptableObject
+    {
+
+        public virtual void Initialize()
+        {
+
+        }
+
+        public virtual void Destroy()
+        {
+
+        }
+    }
+}
